e7b5ce440ed2b68ac6deef549f34a1cbd6834db5
EduardoTADS <guilhermesilva.bg003@academico.ifsul.edu.br>
2024-02-26 21:23:51 -0300

    commit aula01 Images

--- aula01/App.js
+++ aula01/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Olá mundo</Text>
       <TextInput defaultValue="Digite aqui" />
+
+      <Image
+        source={require("./assets/icaro.png")}
+        style={{ width: 150, height: 200 }}
+      />
+      <Image
+        source={require("./assets/teste.gif")}
+        style={{ width: 350, height: 300 }}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
